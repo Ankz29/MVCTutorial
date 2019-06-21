4309efb54e02c7632291540fc11f5e813165752f
Ankita <ankita.m@softwaysolutions.com>
2019-06-21 23:59:22 +0530

    Adding code for Model Binding in ASP.net

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCTutorial.Models;
 
 namespace MVCTutorial.Controllers
 {
@@ -12,14 +13,27 @@ namespace MVCTutorial.Controllers
         // GET: /Test/
         public ActionResult Index()
         {
-            List<string> myList = new List<string>();
-            myList.Add("Ankita");
-            myList.Add("Anisha");
-            myList.Add("Bhagyashree");
-            myList.Add("Bhaveshawari");
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee(){
+                ID = 1, Name = "Ankita", Department ="IT"});
+            new Employee(){
+                 ID = 2, Name = "Anisha", Department ="IT"};
+            new Employee(){
+                 ID = 3, Name = "Bhagyashree", Department ="IT"};
+            new Employee(){
+                 ID = 4, Name = "Bhaveshawari", Department ="IT"};
 
-            ViewBag.NameList = myList;
-            return View();
+            return View(empList);
+            }
+            
+
+            //List<string> myList = new List<string>();
+            //myList.Add("Ankita");
+            //myList.Add("Anisha");
+            //myList.Add("Bhagyashree");
+            //myList.Add("Bhaveshawari");
+
+            //ViewBag.NameList = myList;
+            
         }
 	}
-}
