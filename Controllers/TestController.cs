1a706b9566e1da30d875879b324dbd8d16091e04
Ankita <ankita.m@softwaysolutions.com>
2019-06-21 21:31:41 +0530

    Adding code functionality for ViewBag.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            List<string> myList = new List<string>();
+            myList.Add("Ankita");
+            myList.Add("Anisha");
+            myList.Add("Bhagyashree");
+            myList.Add("Bhaveshawari");
+
+            ViewBag.NameList = myList;
+            return View();
+        }
+	}
+}
